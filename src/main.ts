e86cba515f7a0590589f945c4dbe63ad74992c20
Wolvan <wolvan1@gmail.com>
2021-04-11 15:40:37 +0200

    Prepare Package for npm publishing
    Create an importable main file as well as set up a pre-publish script to
    make sure the entire project passes all tests and builds properly.
    The package name will also be scoped to a `nodejsbots` organisation.

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+"use strict";
+
+import CommandContainer from "./Commands";
+import PluginLoader from "./Pluginloader3";
+import Shell from "./Shell";
+import Storage from "./Storage";
+
+export {
+    CommandContainer,
+    Shell,
+    Storage,
+    PluginLoader
+};
